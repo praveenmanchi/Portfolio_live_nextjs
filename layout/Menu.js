d033d8f75802ad7269dd2936451af1461ad3d808
Praveen manchi <praveenmanchi@proton.me>
2024-02-11 18:13:35 +0530

    Add click tracking to OnePageMenu links

--- layout/Menu.js
+++ layout/Menu.js
@@ -1,120 +1,55 @@
 import Link from "next/link";
+import { Analytics } from '@vercel/analytics/react';
 
-const DropdownBtn = () => (
-  <div className="dropdown-btn">
-    <span className="fas fa-chevron-down" />
-  </div>
-);
-const MultiMenu = () => {
-  return (
-    <ul className="navigation clearfix">
-      <li className="dropdown">
-        <a href="#">Home</a>
-        <ul>
-          <li className="dropdown">
-            <a href="#">MultiPage</a>
-            <ul>
-              <li>
-                <Link href="/">Home One</Link>
-              </li>
-              <li>
-                <Link href="/index2">Home Two</Link>
-              </li>
-            </ul>
-            <DropdownBtn />
-          </li>
-          <li className="dropdown">
-            <a href="#">OnePage</a>
-            <ul>
-              <li>
-                <Link href="/indexonepage">Home One</Link>
-              </li>
-              <li>
-                <Link href="/index2onepage">Home Two</Link>
-              </li>
-            </ul>
-            <DropdownBtn />
-          </li>
-        </ul>
-        <DropdownBtn />
-      </li>
-      <li>
-        <Link href="/about">about</Link>
-      </li>
-      <li>
-        <Link href="/services">services</Link>
-      </li>
-      <li className="dropdown">
-        <a href="#">projects</a>
-        <ul>
-          <li>
-            <Link href="/projects">project Grid</Link>
-          </li>
-          <li>
-            <Link href="/projects-masonry">projects masonry</Link>
-          </li>
-          <li>
-            <Link href="/project-details">projects details</Link>
-          </li>
-        </ul>
-        <DropdownBtn />
-      </li>
-      <li className="dropdown">
-        <a href="#">blog</a>
-        <ul>
-          <li>
-            <Link href="/blog">blog standard</Link>
-          </li>
-          <li>
-            <Link href="/blog-details">blog details</Link>
-          </li>
-        </ul>
-        <DropdownBtn />
-      </li>
-      <li className="dropdown">
-        <a href="#">pages</a>
-        <ul>
-          <li>
-            <Link href="/404">Error Page</Link>
-          </li>
-        </ul>
-        <DropdownBtn />
-      </li>
-      <li>
-        <Link href="contact">Contact</Link>
-      </li>
-    </ul>
-  );
-};
-export default MultiMenu;
 
 export const OnePageMenu = () => {
+  const trackClick = (event, href) => {
+    // Track the click event here
+    console.log(`Clicked on ${href}`);
+  };
+
   return (
     <ul className="navigation onepage clearfix">
       <li>
-        <a href="/">Home</a>
+        <a href="/" onClick={(event) => trackClick(event, "/")}>
+          Home
+        </a>
       </li>
       <li>
-        <a href="#about">about</a>
+        <a href="#about" onClick={(event) => trackClick(event, "#about")}>
+          about
+        </a>
       </li>
       <li>
-        <a href="#resume">Resume</a>
+        <a href="#resume" onClick={(event) => trackClick(event, "#resume")}>
+          Resume
+        </a>
       </li>
       {/* <li>
-        <a href="#services">services</a>
+        <a href="#services" onClick={(event) => trackClick(event, "#services")}>
+          services
+        </a>
       </li> */}
       <li>
-        <a href="#skills">skills</a>
+        <a href="#skills" onClick={(event) => trackClick(event, "#skills")}>
+          skills
+        </a>
       </li>
       <li>
-        <a href="/Casestudies">Casestudies</a>
+        <a href="/Casestudies" onClick={(event) => trackClick(event, "/Casestudies")}>
+          Casestudies
+        </a>
       </li>
       {/* <li>
-        <a href="#blog">blog</a>
+        <a href="#blog" onClick={(event) => trackClick(event, "#blog")}>
+          blog
+        </a>
       </li> */}
-      <li>
-        <a href="#Footer">Contact</a>
-      </li>
+      {/* <li>
+        <a href="#Footer" onClick={(event) => trackClick(event, "#Footer")}>
+          Contact
+        </a>
+      </li> */}
     </ul>
   );
 };
