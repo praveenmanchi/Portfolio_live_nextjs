4bce1dbc619f415da6881e73adce68af6801514e
Praveen manchi <praveenmanchi@proton.me>
2024-02-11 20:27:36 +0530

    updated projects page by un-commenting the project-2 component

--- components/Projects.js
+++ components/Projects.js
@@ -147,168 +147,168 @@ const Projects = () => {
 };
 export default Projects;
 
-// export const Projects2 = () => {
-//   return (
-//     <section
-//       id="projects"
-//       className="projects-area-four pt-135 rpt-100 pb-100 rpb-70 rel z-1"
-//     >
-//       <div className="container container-1200">
-//         <div className="row justify-content-center">
-//           <div className="col-xl-12">
-//             <div className="section-title text-center mb-50 wow fadeInUp delay-0-2s">
-//               <span className="sub-title mb-15">
-//                 <i className="flaticon-asterisk-1" /> Explore my projects
-//               </span>
-//               <h2>Explore My Projects</h2>
-//             </div>
-//           </div>
-//         </div>
-//         <div className="row gap-50">
-//           <div className="col-md-6">
-//             <div className="project-item style-four wow fadeInUp delay-0-2s">
-//               <div className="image">
-//                 <img
-//                   src="assets/images/projects/project-two1.jpg"
-//                   alt="Project"
-//                 />
-//                 <Link legacyBehavior href="/project-details">
-//                   <a className="hover-box">
-//                     <span>View More</span>
-//                     <i className="far fa-arrow-up" />
-//                   </a>
-//                 </Link>
-//               </div>
-//               <div className="content">
-//                 <h5 className="title">
-//                   <Link legacyBehavior href="/project-details">
-//                     <a>Digital Marketing</a>
-//                   </Link>
-//                 </h5>
-//                 <span className="sub-title">Art , Direction</span>
-//               </div>
-//             </div>
-//           </div>
-//           <div className="col-md-6">
-//             <div className="project-item style-four wow fadeInUp delay-0-2s">
-//               <div className="image">
-//                 <img
-//                   src="assets/images/projects/project-two2.jpg"
-//                   alt="Project"
-//                 />
-//                 <Link legacyBehavior href="/project-details">
-//                   <a className="hover-box">
-//                     <span>View More</span>
-//                     <i className="far fa-arrow-up" />
-//                   </a>
-//                 </Link>
-//               </div>
-//               <div className="content">
-//                 <h5 className="title">
-//                   <Link legacyBehavior href="/project-details">
-//                     <a>Digital Marketing</a>
-//                   </Link>
-//                 </h5>
-//                 <span className="sub-title">Art , Direction</span>
-//               </div>
-//             </div>
-//           </div>
-//           <div className="col-md-6">
-//             <div className="project-item style-four wow fadeInUp delay-0-2s">
-//               <div className="image">
-//                 <img
-//                   src="assets/images/projects/project-two3.jpg"
-//                   alt="Project"
-//                 />
-//                 <Link legacyBehavior href="/project-details">
-//                   <a className="hover-box">
-//                     <span>View More</span>
-//                     <i className="far fa-arrow-up" />
-//                   </a>
-//                 </Link>
-//               </div>
-//               <div className="content">
-//                 <h5 className="title">
-//                   <Link legacyBehavior href="/project-details">
-//                     <a>Digital Marketing</a>
-//                   </Link>
-//                 </h5>
-//                 <span className="sub-title">Art , Direction</span>
-//               </div>
-//             </div>
-//           </div>
-//           <div className="col-md-6">
-//             <div className="project-item style-four wow fadeInUp delay-0-2s">
-//               <div className="image">
-//                 <img
-//                   src="assets/images/projects/project-two4.jpg"
-//                   alt="Project"
-//                 />
-//                 <Link legacyBehavior href="/project-details">
-//                   <a className="hover-box">
-//                     <span>View More</span>
-//                     <i className="far fa-arrow-up" />
-//                   </a>
-//                 </Link>
-//               </div>
-//               <div className="content">
-//                 <h5 className="title">
-//                   <Link legacyBehavior href="/project-details">
-//                     <a>Digital Marketing</a>
-//                   </Link>
-//                 </h5>
-//                 <span className="sub-title">Art , Direction</span>
-//               </div>
-//             </div>
-//           </div>
-//           <div className="col-md-6 order-md-3">
-//             <div className="project-item style-four wow fadeInUp delay-0-2s">
-//               <div className="image">
-//                 <img
-//                   src="assets/images/projects/project-two5.jpg"
-//                   alt="Project"
-//                 />
-//                 <Link legacyBehavior href="/project-details">
-//                   <a className="hover-box">
-//                     <span>View More</span>
-//                     <i className="far fa-arrow-up" />
-//                   </a>
-//                 </Link>
-//               </div>
-//               <div className="content">
-//                 <h5 className="title">
-//                   <Link legacyBehavior href="/project-details">
-//                     <a>Digital Marketing</a>
-//                   </Link>
-//                 </h5>
-//                 <span className="sub-title">Art , Direction</span>
-//               </div>
-//             </div>
-//           </div>
-//           <div className="col-md-6 align-self-center">
-//             <div className="project-details-btn mb-50">
-//               <Link legacyBehavior href="/project-details">
-//                 <a className="more-btn wow zoomIn delay-0-2s">
-//                   <span>More</span>
-//                   <i className="fal fa-arrow-up" />
-//                 </a>
-//               </Link>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//       <div className="bg-lines">
-//         <span />
-//         <span />
-//         <span />
-//         <span />
-//         <span />
-//         <span />
-//         <span />
-//         <span />
-//         <span />
-//         <span />
-//       </div>
-//     </section>
-//   );
-// };
+export const Projects2 = () => {
+  return (
+    <section
+      id="projects"
+      className="projects-area-four pt-135 rpt-100 pb-100 rpb-70 rel z-1"
+    >
+      <div className="container container-1200">
+        <div className="row justify-content-center">
+          <div className="col-xl-12">
+            <div className="section-title text-center mb-50 wow fadeInUp delay-0-2s">
+              <span className="sub-title mb-15">
+                <i className="flaticon-asterisk-1" /> Explore my projects
+              </span>
+              <h2>Explore My Projects</h2>
+            </div>
+          </div>
+        </div>
+        <div className="row gap-50">
+          <div className="col-md-6">
+            <div className="project-item style-four wow fadeInUp delay-0-2s">
+              <div className="image">
+                <img
+                  src="assets/images/projects/project-two1.jpg"
+                  alt="Project"
+                />
+                <Link legacyBehavior href="/project-details">
+                  <a className="hover-box">
+                    <span>View More</span>
+                    <i className="far fa-arrow-up" />
+                  </a>
+                </Link>
+              </div>
+              <div className="content">
+                <h5 className="title">
+                  <Link legacyBehavior href="/project-details">
+                    <a>Digital Marketing</a>
+                  </Link>
+                </h5>
+                <span className="sub-title">Art , Direction</span>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="project-item style-four wow fadeInUp delay-0-2s">
+              <div className="image">
+                <img
+                  src="assets/images/projects/project-two2.jpg"
+                  alt="Project"
+                />
+                <Link legacyBehavior href="/project-details">
+                  <a className="hover-box">
+                    <span>View More</span>
+                    <i className="far fa-arrow-up" />
+                  </a>
+                </Link>
+              </div>
+              <div className="content">
+                <h5 className="title">
+                  <Link legacyBehavior href="/project-details">
+                    <a>Digital Marketing</a>
+                  </Link>
+                </h5>
+                <span className="sub-title">Art , Direction</span>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="project-item style-four wow fadeInUp delay-0-2s">
+              <div className="image">
+                <img
+                  src="assets/images/projects/project-two3.jpg"
+                  alt="Project"
+                />
+                <Link legacyBehavior href="/project-details">
+                  <a className="hover-box">
+                    <span>View More</span>
+                    <i className="far fa-arrow-up" />
+                  </a>
+                </Link>
+              </div>
+              <div className="content">
+                <h5 className="title">
+                  <Link legacyBehavior href="/project-details">
+                    <a>Digital Marketing</a>
+                  </Link>
+                </h5>
+                <span className="sub-title">Art , Direction</span>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="project-item style-four wow fadeInUp delay-0-2s">
+              <div className="image">
+                <img
+                  src="assets/images/projects/project-two4.jpg"
+                  alt="Project"
+                />
+                <Link legacyBehavior href="/project-details">
+                  <a className="hover-box">
+                    <span>View More</span>
+                    <i className="far fa-arrow-up" />
+                  </a>
+                </Link>
+              </div>
+              <div className="content">
+                <h5 className="title">
+                  <Link legacyBehavior href="/project-details">
+                    <a>Digital Marketing</a>
+                  </Link>
+                </h5>
+                <span className="sub-title">Art , Direction</span>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6 order-md-3">
+            <div className="project-item style-four wow fadeInUp delay-0-2s">
+              <div className="image">
+                <img
+                  src="assets/images/projects/project-two5.jpg"
+                  alt="Project"
+                />
+                <Link legacyBehavior href="/project-details">
+                  <a className="hover-box">
+                    <span>View More</span>
+                    <i className="far fa-arrow-up" />
+                  </a>
+                </Link>
+              </div>
+              <div className="content">
+                <h5 className="title">
+                  <Link legacyBehavior href="/project-details">
+                    <a>Digital Marketing</a>
+                  </Link>
+                </h5>
+                <span className="sub-title">Art , Direction</span>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6 align-self-center">
+            <div className="project-details-btn mb-50">
+              <Link legacyBehavior href="/project-details">
+                <a className="more-btn wow zoomIn delay-0-2s">
+                  <span>More</span>
+                  <i className="fal fa-arrow-up" />
+                </a>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-lines">
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+      </div>
+    </section>
+  );
+};
