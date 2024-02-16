62985055986c7fc0dfbfd0ab597ac5e6ba8ab1e0
Praveen manchi <praveenmanchi@proton.me>
2024-02-16 13:40:21 +0530

    changed og image link

--- app/Casestudies/page.js
+++ app/Casestudies/page.js
@@ -16,7 +16,7 @@ export const metadata = {
     default:"Praveen Manchi || Casestudies", // a default is required when creating a template
   },
   openGraph: {
-    images: ['https://www.istockphoto.com/photo/concepts-of-laser-eye-surgery-or-visual-acuity-check-up-gm1083316464-290625254'],
+    images: ['https://media.istockphoto.com/id/1399246824/photo/digital-eye-wave-lines-stock-background.jpg?s=1024x1024&w=is&k=20&c=429ls1h7nmRkKERaJdq6TLaO1H0eSQHLgYXUXkPjJDI='],
   },
   description: "Praveen's Portfolio - Explore the creative works and projects in UX/UI design, web development, and more.",
 };
