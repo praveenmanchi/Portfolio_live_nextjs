db4b1bae101778898bcad04e387f4b2147e7f971
praveenkumar <ppraveenk07@gmail.com>
2024-02-16 12:38:14 +0530

    OG image added, testing need to done

--- app/page.js
+++ app/page.js
@@ -1,21 +1,25 @@
 import About from "@/components/About";
-import Blog from "@/components/Blog";
+// import Blog from "@/components/Blog";
 import Clients from "@/components/Clients";
-import Contact from "@/components/Contact";
+// import Contact from "@/components/Contact";
 import Hero from "@/components/Hero";
-import Pricing from "@/components/Pricing";
+// import Pricing from "@/components/Pricing";
 import Projects from "@/components/Projects";
 import Resume from "@/components/Resume";
-import Services from "@/components/Services";
+// import Services from "@/components/Services";
 import Skill from "@/components/Skill";
 import ScrollTop from "@/components/ScrollTop";
 import Testimonial from "@/components/Testimonial";
 import NoxfolioLayout from "@/layout/NoxfolioLayout";
+import Head from 'next/head';
 
 export const metadata = {
   title: {
     template: "Praveen Manchi || %s",
     default:"Praveen Manchi || Home", // a default is required when creating a template
+  },
+  openGraph: {
+    images: ['https://www.istockphoto.com/photo/digital-eye-wave-lines-stock-background-gm1399246824-453166593'],
   },
   description: "Praveen's Portfolio - Explore the creative works and projects in UX/UI design, web development, and more.",
 };
@@ -24,6 +28,7 @@ export const metadata = {
 export default function HomeOnePage() {
   return (
     <NoxfolioLayout onePageMenu={true}>
+     
       {/* Hero Section Start */}
       <Hero />
       {/* Hero Section End */}
